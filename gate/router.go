bdbd42bd4299a06dc8f52f782079abba421de378
aris song <mrsong0607@gmail.com>
2017-07-11 17:58:45 +0800

    add test code

--- gate/router.go
+++ gate/router.go
@@ -1,5 +1,12 @@
 package gate
 
-func init() {
+import (
+	"Leaf4Game/game"
+	"Leaf4Game/msg"
+)
 
+func init() {
+	// 这里指定消息 Hello 路由到 game 模块
+	// 模块间使用 ChanRPC 通讯，消息路由也不例外
+	msg.Processor.SetRouter(&msg.Hello{}, game.ChanRPC)
 }
--- msg/msg.go
+++ msg/msg.go
@@ -1,11 +1,15 @@
 package msg
 
 import (
-	"github.com/name5566/leaf/network"
+	"github.com/name5566/leaf/network/json"
 )
 
-var Processor network.Processor
+var Processor = json.NewProcessor()
 
 func init() {
+	Processor.Register(&Hello{})
+}
 
+type Hello struct {
+	Name string
 }
